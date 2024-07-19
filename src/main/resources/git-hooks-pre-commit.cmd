d7543e57a0c701808c4383c93d1829033c8ecae9
green <46670613+greenn-lab@users.noreply.github.com>
2024-07-19 17:34:12 +0900

    🩹checkstyle error log filter pattern

--- src/main/resources/git-hooks-pre-commit.cmd
+++ src/main/resources/git-hooks-pre-commit.cmd
@@ -3,7 +3,7 @@
 echo "    ✅ 커밋 규칙을 검사합니다. "
 
 unset JAVA_TOOL_OPTIONS
-./mvnw.cmd -Dfile.encoding=utf-8 checkstyle:check | grep -E "(^\[ERROR].+\..+:[0-9]+:[0-9]+.+$)|(^\[INFO] BUILD FAILURE$)"
+./mvnw.cmd -Dfile.encoding=utf-8 checkstyle:check | grep -E "(^\[ERROR].+\..+:[0-9]+:.+$)|(^\[INFO] BUILD FAILURE$)"
 
 
 RESULTS=$?
